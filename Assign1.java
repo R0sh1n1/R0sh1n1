2941bbc886e55588fbf0264ccae0082ec2ae38ce
ROsh1n1 <bimarasettyroshini@gmail.com>
2025-04-14 17:06:17 +0530

    commit message

---
+++ Assign1.java
@@ -0,0 +1,28 @@
+package Git;
+
+public class Assign1 {
+    public static void main(String[] args) {
+        
+        int a=10;
+        System.out.println(a+=10);
+        int b=120;
+        System.out.println(b-=25);
+        int c=35;
+        System.out.println(c*=5);
+        int d=20;
+        System.out.println(d/=2);
+        int e=10;
+        System.out.println(e%=2);
+        int x=200;
+        System.out.println(x+=20);
+        System.out.println(x-=30);
+        System.out.println(x*=2);
+        System.out.println(x/=4);
+        System.out.println(x%=2);
+
+
+    }
+    
+}
+    
+
